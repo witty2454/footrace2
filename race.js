8ad6a9afe923e91951137c7efe15746b36bb338e
witty2454 <51719341+witty2454@users.noreply.github.com>
2019-06-17 12:58:49 -0500

    These are my initial up loads of my files.

---
+++ race.js
@@ -0,0 +1,12 @@
+// As a devopler I want to maek good, consistant commits.
+// As a player, I want to be able to challenge another player to a race to the finish.
+// As a player, I am furnished with a set of rules including the various dice and their values.
+
+
+// As a player, I will roll a 20-sided die along with my opponet to determinewho goes first.
+// I need a function to roll a 20-sided die and to determine the higher number rolled, in which the highest rolled goes first.
+
+//*********************************** Player Turn...
+
+///As a player, I will see that I was given $8  credit at the being of the game to make first move.
+// As a player, I can use my credit to purchase
