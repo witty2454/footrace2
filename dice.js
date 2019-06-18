edd8a2a3b7e9ccc79afbdb24fa6081cfc3c3871c
witty2454 <51719341+witty2454@users.noreply.github.com>
2019-06-18 01:20:45 -0500

    This changes come with adding a second Java Script page and also bringing some CSS.
    These changes add a second JS page to deal with the functions of the dice themselves. I also added a CSS page so there was a little more to the screen. Both will be updated regularly  with the other pages.

---
+++ dice.js
@@ -0,0 +1,56 @@
+class DiceSet {
+    constructor() {
+      this.sides = 8;
+      this.quantity = 3;
+      this.history = [];
+      this.runningTotal = 0;
+    }
+  
+    singleRoll() {
+      return Math.floor(Math.random() * this.sides + 1);
+    }
+  
+    setNumSides(numSides) {
+      this.sides = numSides;
+      this.reset();
+    }
+  
+    setDiceSetSize(numDice) {
+      this.quantity = numDice;
+      this.reset();
+    }
+  
+    reset() {
+      this.history = [];
+      this.runningTotal = 0;
+    }
+  
+    numRolls() {
+      return this.history.length;
+    }
+  
+    getAverage() {
+      return this.runningTotal / this.history.length;
+    }
+  
+    roll() {
+      var total, same, rollSet, i;
+      same = true;
+      rollSet = [];
+      rollSet[0] = this.singleRoll();
+      total = rollSet[0];
+      for (i = 1; i < this.quantity; i++) {
+        rollSet[i] = this.singleRoll();
+        total += rollSet[i];
+        if (rollSet[i] !== rollSet[i-1]) {
+          same = false;
+        }
+      }
+      this.history.push(total);
+      this.runningTotal += total;
+      return rollSet;
+      
+  
+    }
+  }
+  
